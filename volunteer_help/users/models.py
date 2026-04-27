4f4c160e505fdca5282a1e695e5703efc0d7c37a
unknown <main@example.com>
2026-04-27 10:18:57 +0300

    Модель пользователя и менеджер

--- volunteer_help/users/models.py
+++ volunteer_help/users/models.py
@@ -1,8 +1,61 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
+from django.contrib.auth.hashers import make_password
 
 
-class MyUser(AbstractUser):
+class MyUserManager(BaseUserManager):
+    def _create_user_object(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        return user
+
+    def _create_user(self, email, password, **extra_fields):
+        user = self._create_user_object(email, password, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField("email address", unique=True, blank=True)
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_active = models.BooleanField(
+        "active",
+        default=True,
+        help_text=
+        "Designates whether this user should be treated as active. "
+        "Unselect this instead of deleting accounts."
+    ),
+    date_joined = models.DateTimeField("date joined", default=timezone.now)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
